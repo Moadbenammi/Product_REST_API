73df7e82af1f8f28fa42bf8332358affc10dc98b
unknown <moadbenammi@gmail.com>
2021-07-13 15:19:15 +0100

    Improve code

--- app/controllers/product.js
+++ app/controllers/product.js
@@ -1,51 +1,56 @@
-const Product = require('../models/product');
-const fs = require('fs');
+const Product = require("../models/product");
 
 exports.createProduct = (req, res, next) => {
-    const productObject = JSON.parse(req.body.product);
-    delete productObject._id;
-    const product = new Product({
-        ...productObject,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    });
-    product.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré' }))
-        .catch(error => res.status(400).json({ error }));
+  const { title, description, imageUrl, userId, price } = req.body;
+  const product = new Product({
+    title,
+    description,
+    imageUrl,
+    userId,
+    price,
+  });
+  product
+    .save()
+    .then(() => res.status(201).json({ message: "Product was saved !" }))
+    .catch((error) => res.status(400).json({ error }));
 };
 
 exports.updateProduct = (req, res, next) => {
-    const productObject = req.file ? 
-        {
-          ...JSON.parse(req.body.product),
-          imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
-        }
-      : { ...req.body };
-    Product.updateOne({ _id: req.params.id }, { ...productObject, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié' }))
-        .catch(error => res.status(400).json({ error }));
+  Product.updateOne(
+    {
+      _id: req.params.id,
+    },
+    req.body
+  )
+    .then(() =>
+      res.status(201).send({ message: "Product updated successfully!" })
+    )
+    .catch(() =>
+      res.status(404).json({
+        error: "Product not found",
+      })
+    );
 };
 
 exports.deleteProduct = (req, res, next) => {
-    Product.findOne({ _id: req.params.id })
-        .then(product => {
-            const filename = product.imageUrl.split('/images/')[1];
-            fs.unlink(`images/${filename}`, () => {
-                Product.deleteOne({ _id: req.params.id })
-                  .then(() => res.status(200).json({ message: "Objet supprimé" }))
-                  .catch((error) => res.status(400).json({ error }));
-            });
-        })
-        .catch(error => res.status(500).json({ error }));    
+  Product.findOne({ _id: req.params.id })
+    .then((product) => {
+      product
+        .deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: "Product was deleted !" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch(() => res.status(500).json({ error: "Product not found!" }));
 };
 
 exports.getOneProduct = (req, res, next) => {
-    Product.findOne({ _id: req.params.id })
-        .then(product => res.status(200).json(product))
-        .catch(error => res.status(404).json({ error }));
+  Product.findOne({ _id: req.params.id })
+    .then((product) => res.status(200).json(product))
+    .catch((error) => res.status(404).json({ error }));
 };
 
 exports.getAllProducts = (req, res, next) => {
-    Product.find()
-        .then(products => res.status(200).json(products))
-        .catch(error => res.status(400).json({ error }));
+  Product.find()
+    .then((products) => res.status(200).json(products))
+    .catch((error) => res.status(400).json({ error }));
 };
